010a02154f5222ff5ade9af9bae64d59231c2a35
Drahov <78694359+Drahov@users.noreply.github.com>
2021-05-25 15:56:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,100 @@
-const fs = require('fs')
+const fs = require('fs');
 
-class DB {
+class Database {
 
-   constructor(){
-
+   constructor(filename){
+       this.filename = filename;
+    }
+   add(dataname,value){
+       try{JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf-8'))}catch(e){throw new TypeError(`Unavailable file ${this.filename}.json \n` + __dirname)}
+       var file = JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf8'))
+       if(!value){
+        throw new TypeError("Expected data\n" + __dirname)
+       }
+       if(!this.filename){
+        throw new TypeError("Please type file name\n" + __dirname)
+       }
+       file[dataname] = value
+       return fs.writeFileSync(`${this.filename}.json` , JSON.stringify(file, null, 2))
    }
-   write(veri,değer,fileName){
-       const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-       dosya[veri] = değer
-       return fs.writeFileSync(`${fileName}.json` , JSON.stringify(dosya, null, 1))
-       if(!veri) throw new TypeError("Expected data\n" + __dirname)
-       if(!fileName) throw new TypeError("Please type file name\n" + __dirname)
+   get(dataname){
+       try{JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf-8'))}catch(e){throw new TypeError(`Unavailable file ${this.filename}.json \n` + __dirname)}
+       var file = JSON.parse(fs.readFileSync(`${this.filename}.json`,'utf8'))
+       if(!file[dataname]){
+        throw new TypeError('The data you typed could not be found\n' + __dirname)
+       }
+       if(!dataname){
+        throw new TypeError("Expected data\n" + __dirname)
+       }
+       return file[dataname]
    }
-   get(veri,fileName){
-       const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`,'utf8'))
-       if(!dosya[veri]) throw new TypeError('The data you typed could not be found\n' + __dirname)
-       if(!veri) throw new TypeError("Expected data\n" + __dirname)
-       return dosya[veri]
+   has(dataname){
+       try{JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf-8'))}catch(e){throw new TypeError(`Unavailable file ${this.filename}.json \n` + __dirname)}
+       var file = JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf8'))
+       if(!dataname){
+        throw new TypeError("Expected data\n" + __dirname) 
+       }     
+       return file[dataname] ? true : false
    }
-   has(veri,fileName){
-       const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-       if(!veri) throw new TypeError("Expected data\n" + __dirname)    
-       return dosya[veri] ? true : false
-   }
-   delete(veri,fileName){
-    const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-    if(!dosya[veri]) throw new TypeError('The data you typed could not be found\n' + __dirname)
-    if(!veri) throw new TypeError("Expected data\n" + __dirname)
-    delete dosya[veri]
-    return fs.writeFileSync('database.json', JSON.stringify(dosya,null,2))  
+   remove(dataname){
+    try{JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf-8'))}catch(e){throw new TypeError(`Unavailable file ${this.filename}.json \n` + __dirname)}
+    var file = JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf8'))
+    if(!file[dataname]){
+       throw new TypeError('The data you typed could not be found\n' + __dirname)
+    }
+    if(!dataname){
+        throw new TypeError("Expected data\n" + __dirname)
+    }
+    delete file[dataname]
+    return fs.writeFileSync(`${this.filename}.json`, JSON.stringify(file,null,2))  
    } 
-   backup(dosyaAdı,fileName){
-    const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-    return fs.writeFileSync(`${dosyaAdı}.json`, JSON.stringify(dosya,null,2))
-    if(!veri) throw new TypeError("Expected data\n"  + __dirname)
-    if(!değer) throw new TypeError("Expected value\n" + __dirname)
+   copy(fileName){
+       try{var file = JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf-8'))}catch(e){throw new TypeError(`Unavailable file ${this.filename}.json \n` + __dirname)}
+       if(!fileName){
+        throw new TypeError("Please type a filename")
+       }
+       return fs.writeFileSync(`${fileName}.json`, JSON.stringify(file, null, 2))
    }
-   plus(veri,değer,fileName){
-       if(!veri) throw new TypeError("Expected data\n" + __dirname)
-       if(typeof değer !== 'number') throw new TypeError("Please type a number\n" + __dirname)
-       if(!this.get(veri)) throw new TypeError("The data you typed could not be found\n" + __dirname)
-       if(typeof this.find(veri) !== 'number') throw new TypeError("Please type number value\n" + __dirname)
-       const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-       dosya[veri] += değer
-       return fs.writeFileSync(`${fileName}.json`, JSON.stringify(dosya,null,2))
+   increase(dataname,value){
+       try{
+           JSON.parse(fs.readFileSync(`${this.filename}.json`,'utf-8'))
+       }catch(e){
+           throw new TypeError(`Unavailable file ${this.filename}.json\n${__dirname}`)
+       }
+       var file = JSON.parse(fs.readFileSync(`${this.filename}.json`,'utf-8'))
+       if(!dataname) throw new TypeError('Type a data key\n' + __dirname)
+       if(typeof value !== 'number') throw new TypeError("Please type a number as value")
+       if(!this.has(dataname)) throw new TypeError('The data typed couldnt be found')
+       if(typeof this.get(dataname) !== 'number') throw new TypeError('Only can increase a number')
+       file[dataname] += value
+       return fs.writeFileSync(`${this.filename}.json`,JSON.stringify(file,null,2))
    }
-   minus(veri,değer,fileName){
-    if(!veri) throw new TypeError("Expected data\n" + __dirname)
-    if(typeof değer !== 'number') throw new TypeError("Please type a number\n" + __dirname)
-    if(!this.control(veri)) throw new TypeError("The data you typed could not be found\n" + __dirname)
-    if(typeof this.find(veri) !== 'number') throw new TypeError("Please type number value\n" + __dirname)
-    const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-    dosya[veri] -= değer
-    return fs.writeFileSync(`${fileName}.json`, JSON.stringify(dosya,null,2))
+   reduce(dataname,value){
+    try{
+        JSON.parse(fs.readFileSync(`${this.filename}.json`,'utf-8'))
+    }catch(e){
+        throw new TypeError(`Unavailable file ${this.filename}.json\n${__dirname}`)
+    }
+    var file = JSON.parse(fs.readFileSync(`${this.filename}.json`,'utf-8'))
+    if(!dataname) throw new TypeError('Type a data key\n' + __dirname)
+    if(typeof value !== 'number') throw new TypeError("Please type a number as value")
+    if(!this.has(dataname)) throw new TypeError('The data typed couldnt be found')
+    if(typeof this.get(dataname) !== 'number') throw new TypeError('Only can reduce a number')
+    file[dataname] -= value
+    return fs.writeFileSync(`${this.filename}.json`,JSON.stringify(file,null,2))
    }
-   deleteall(fileName){
-    const dosya = JSON.parse(fs.readFileSync(`${fileName}.json`, 'utf8'))
-    return fs.writeFileSync(`${fileName}.json`, JSON.stringify(dosya,null,2))
+   clear(){
+    try{
+        JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf8'))
+    }catch(e){
+        //throw new TypeError(`Unavailable file ${this.filename}.json\n${__dirname}`)
+        console.log(e)
+    }
+    var file = JSON.parse(fs.readFileSync(`${this.filename}.json`, 'utf8'))
+    return fs.writeFileSync(`${this.filename}.json`, JSON.stringify({},null,2))
    }
 }
 
 
 
-module.exports = new DB()
+module.exports.Database = Database
